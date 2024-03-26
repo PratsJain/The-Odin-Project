4853b45b8882c1d94ad6405705427629e2c637f5
Pratyush Jain <pratyush20396@iiitd.ac.in>
2024-03-26 17:47:49 +0530

    rest-page: home and about page done

--- restaurant-page/src/about.js
+++ restaurant-page/src/about.js
@@ -1,19 +1,38 @@
-export default function about() {
+import chefimg from './assets/chef.png';
+
+export default function home() {
     document.getElementById("home").setAttribute("class", "unselected");
     document.getElementById("menu").setAttribute("class", "unselected");
     document.getElementById("about").setAttribute("class", "selected");
     const cont = document.getElementById("container");
     cont.innerHTML = "";
-    const titleBox = document.createElement("div");
-    titleBox.setAttribute("class", "title-box");
-    const title = document.createElement("p");
-    title.textContent = "Spice Paradise of India";
-    titleBox.appendChild(title);
-    const introbox = document.createElement("div");
-    introbox.setAttribute("class", "content-box");
-    const intro = document.createElement("p");
-    intro.textContent = "Embark on a tantalizing journey through the vibrant flavors of India at 'Spice Paradise,' our pure veg haven. Indulge in aromatic curries, delicate dosas, and tantalizing tandoori delights meticulously crafted to tantalize your taste buds. Join us in celebrating the rich heritage and culinary artistry of Indian vegetarian cuisine.";
-    introBox.appendChild(intro);
-    cont.appendChild(titleBox);
-    cont.appendChild(introbox);
+    const createEL = (divClass, title, istitle = false, content = null, img = null) => {
+        const box = document.createElement("div");
+        box.setAttribute("class", divClass);
+        const title1 = document.createElement("p");
+        title1.innerText = title;
+        box.appendChild(title1);
+        if (!istitle) {
+            title1.setAttribute("class", "box-title");
+            if (content !== null) {
+                const content1 = document.createElement("p");
+                content1.innerText = content;
+                box.appendChild(content1);
+            }
+            if (img !== null) {
+                const mydp = new Image();
+                mydp.src = img;
+                mydp.setAttribute("class", "name-card-photo");
+                box.appendChild(mydp);
+            }
+        }
+        cont.appendChild(box);
+    };
+    createEL("title-box", "Our Lineup", true);
+    let content = "Chef\n555-555-5554\nmotu@spiceParadise.com";
+    createEL("name-card", "Motabhai", false, content, chefimg);
+    content = "Manager\n555-555-5555\njetha@spiceParadise.com";
+    createEL("name-card", "Jethalal", false, content, chefimg);
+    content = "Waiter\n555-555-5556\niter@spiceParadise.com";
+    createEL("name-card", "Iyer", false, content, chefimg);
 }
--- restaurant-page/src/home.js
+++ restaurant-page/src/home.js
@@ -4,16 +4,27 @@ export default function home() {
     document.getElementById("about").setAttribute("class", "unselected");
     const cont = document.getElementById("container");
     cont.innerHTML = "";
-    const titleBox = document.createElement("div");
-    titleBox.setAttribute("class", "title-box");
-    const title = document.createElement("p");
-    title.textContent = "Spice Paradise of India";
-    titleBox.appendChild(title);
-    const introbox = document.createElement("div");
-    introbox.setAttribute("class", "content-box");
-    const intro = document.createElement("p");
-    intro.textContent = "Embark on a tantalizing journey through the vibrant flavors of India at 'Spice Paradise,' our pure veg haven. Indulge in aromatic curries, delicate dosas, and tantalizing tandoori delights meticulously crafted to tantalize your taste buds. Join us in celebrating the rich heritage and culinary artistry of Indian vegetarian cuisine.";
-    introbox.appendChild(intro);
-    cont.appendChild(titleBox);
-    cont.appendChild(introbox);
+    const createEL = (divClass, title, istitle = false, content = null) => {
+        const box = document.createElement("div");
+        box.setAttribute("class", divClass);
+        const title1 = document.createElement("p");
+        title1.innerText = title;
+        box.appendChild(title1);
+        if (!istitle) {
+            title1.setAttribute("class", "box-title");
+            if (content !== null) {
+                const content1 = document.createElement("p");
+                content1.innerText = content;
+                box.appendChild(content1);
+            }
+        }
+        cont.appendChild(box);
+    };
+    createEL("title-box", "Spice Paradise of India", true);
+    let content = "Embark on a tantalizing journey through the vibrant flavors of India at 'Spice Paradise,' our pure veg haven. Indulge in aromatic curries, delicate dosas, and tantalizing tandoori delights meticulously crafted to tantalize your taste buds. Join us in celebrating the rich heritage and culinary artistry of Indian vegetarian cuisine.";
+    createEL("content-box", "Foundation", false, content);
+    content = "Sunday \xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0 8am - 8pm\nMonday\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa06am - 6pm\nTuesday\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa06am - 6pm\nWednesday\xa0\xa0\xa0\xa0\xa0\xa0\xa06am - 6pm\nThursday\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa06am - 10pm\nFriday \xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0 6am - 10pm\nSaturday\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa08am - 10pm";
+    createEL("content-box", "Timings", false, content);
+    content = "123-A, Connaught Place\nNew Delhi-110001\nBharat";
+    createEL("content-box", "Visit Us", false, content);
 }
