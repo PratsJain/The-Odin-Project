8991120bc1efa86307f596b4d935c67947c5dc25
Pratyush Jain <pratyush20396@iiitd.ac.in>
2024-04-05 14:28:07 +0530

    animation practice exercises

---
+++ practice-exercises/animations/Misc/index.js
@@ -0,0 +1,9 @@
+const divElem = document.querySelector("div");
+const htmlElem = document.querySelector(":root");
+
+htmlElem.addEventListener("click", showHide);
+document.addEventListener("keydown", showHide);
+
+function showHide() {
+  divElem.classList.toggle("showing");
+}
